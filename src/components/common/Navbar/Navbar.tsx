4753d4ddf81c1568e93c2366a045c40841cafb53
alaasassii <alaasassijs@gmail.com>
2023-10-16 11:59:05 +0100

    add style

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,20 +1,26 @@
 import { NavLink } from 'react-router-dom';
 import { MdFastfood } from 'react-icons/md';
+import { BsFillMoonStarsFill, BsSunFill } from 'react-icons/bs'
 import './Navbar.scss'
+import useTheme from '../../../hooks/useTheme';
 const Navbar = () => {
+    const { theme, toggleTheme } = useTheme()
     return (
-        <nav className='container'>
-            <div className="nav__left">
-                <MdFastfood />
-                Recepies
-            </div>
-            <div className="nav__right">
-                <ul>
-                    <li><NavLink to='/'  >Recipes Search</NavLink></li>
-                    <li><NavLink to='/'  >Random Recipe</NavLink></li>
-                    <li><NavLink to='/'  >Category-based Recipe</NavLink></li>
-                    <li><NavLink to='/'  >Advanced Recipe Search</NavLink></li>
-                </ul>
+        <nav>
+            <div className="container">
+                <div className="nav__left">
+                    <MdFastfood />
+                    Recepies
+                </div>
+                <div className="nav__right">
+                    <ul>
+                        <li><NavLink to='/'  >Recipes Search</NavLink></li>
+                        <li><NavLink to='/'  >Random Recipe</NavLink></li>
+                        <li><NavLink to='/'  >Category-based Recipe</NavLink></li>
+                        <li><NavLink to='/'  >Advanced Recipe Search</NavLink></li>
+                    </ul>
+                    <button onClick={toggleTheme}>{theme === 'theme-light' ? <BsSunFill /> : <BsFillMoonStarsFill />}</button>
+                </div>
             </div>
         </nav>
     )
