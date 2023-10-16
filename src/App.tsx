530344d0e50aca48bee61562fd147dc2ff8a579f
alaasassii <alaasassijs@gmail.com>
2023-10-16 11:25:48 +0100

    add home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './routes/Home'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
   )
 }
 
