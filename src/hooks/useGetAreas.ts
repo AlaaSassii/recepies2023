a2636d1be53a73531ecfa4c4c8fd6c1eab744309
alaasassii <alaasassijs@gmail.com>
2023-10-17 14:13:21 +0100

    add new things

---
+++ src/hooks/useGetAreas.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { getAreas } from "../redux/cuisineSlice"
+import { useAppDispatch } from "./useAppDispatch"
+import { useAppSelector } from "./useAppSelector";
+const useGetAreas = () => {
+    const dispatch = useAppDispatch();
+    const { error, loading, value } = useAppSelector(state => state.cuisine.area);
+    useEffect(() => {
+        dispatch(getAreas())
+    }, [])
+    return { value, loading, error }
+}
+
+export default useGetAreas;
