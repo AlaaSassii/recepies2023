2dbdb75e5d910daf975611a9a092899f5bb696ee
alaasassii <alaasassijs@gmail.com>
2023-10-17 14:12:16 +0100

    complete useGetCategories

---
+++ src/hooks/useGetCategories.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { getCategories } from "../redux/cuisineSlice"
+import { useAppDispatch } from "./useAppDispatch"
+import { useAppSelector } from "./useAppSelector";
+const useGetCategories = () => {
+    const dispatch = useAppDispatch();
+    const { error, loading, value } = useAppSelector(state => state.cuisine.category);
+    useEffect(() => {
+        dispatch(getCategories())
+    }, [])
+    return { value, loading, error }
+}
+
+export default useGetCategories;
