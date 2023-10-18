8b24ee9c619970814e1893f397bc3dcddc6e1e87
alaasassii <alaasassijs@gmail.com>
2023-10-18 10:01:53 +0100

    add new thing

--- src/routes/SearchMeal.tsx
+++ src/routes/SearchMeal.tsx
@@ -1,6 +1,7 @@
 import InputSearch from '../components/common/InputSearch'
 import Loading from '../components/common/Loading';
 import PageContainer from '../components/common/PageContainer'
+import RecepieCard from '../components/common/RecepieCard';
 import MealCard from '../components/specify/MealCard';
 import useSearchRecepieByName from '../hooks/useSearchRecepieByName';
 
@@ -19,15 +20,22 @@ const SearchMeal = () => {
                         :
                         <div className='meals__container'>
                             {
-                                recepies?.map((v, index) => <MealCard
-                                    idMeal={v.idMeal}
-                                    strMeal={v.strMeal}
-                                    strMealThumb={v.strMealThumb}
-                                    strArea={v.strArea}
-                                    strCategory={v.strCategory}
-                                    key={`meal___${index}`}
+                                recepies?.map((v, index) =>
+                                    <RecepieCard
+                                        recepieId={v.idMeal}
+                                        image={v.strMealThumb}
+                                        recepieName={v.strMeal}
+                                    />
+                                    //  <MealCard
+                                    //     idMeal={v.idMeal}
+                                    //     strMeal={v.strMeal}
+                                    //     strMealThumb={v.strMealThumb}
+                                    //     strArea={v.strArea}
+                                    //     strCategory={v.strCategory}
+                                    //     key={`meal___${index}`}
 
-                                />)
+                                    // />
+                                )
                             }
                         </div>
 
