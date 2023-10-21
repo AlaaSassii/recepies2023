e0cc014f1f3970c6d661fb4d46945b33e2fa56df
alaasassii <alaasassijs@gmail.com>
2023-10-21 15:29:37 +0100

    update useTheme code

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 
 const useTheme = () => {
     const [theme, setTheme] = useState(() => {
-        // Try to retrieve the theme from localStorage, or use 'theme-light' as the default.
         return localStorage.getItem('theme') || 'theme-light';
     });
 
@@ -13,10 +12,8 @@ const useTheme = () => {
     };
 
     useEffect(() => {
-        // Set the theme attribute on the document's root element
         document.documentElement.setAttribute('data-theme', theme);
 
-        // Save the theme to localStorage
         localStorage.setItem('theme', theme);
     }, [theme]);
 
